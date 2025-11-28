91222b73031dae08fd9b743afcac73f993633aff
VincentB03 <162428778+VincentB03@users.noreply.github.com>
2025-11-28 10:27:02 +0100

    commentaires CNN1.py

--- models/CNN1.py
+++ models/CNN1.py
@@ -5,16 +5,17 @@ import torch.nn.functional as F
 class CNN1(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(2, 32, 3, padding=1)
+        self.conv1 = nn.Conv2d(2, 32, 3, padding=1) #2 canaux : 1 pour l'image, 1 pour le temps
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
         self.conv_out = nn.Conv2d(64, 1, 3, padding=1)
 
     def forward(self, x, t):
-        t = t.view(-1, 1, 1, 1).expand(-1, 1, x.size(2), x.size(3))
+        t = t.view(-1, 1, 1, 1).expand(-1, 1, x.size(2), x.size(3)) #concaténation image et temps
         x_in = torch.cat([x, t], dim=1)
         h = F.relu(self.conv1(x_in))
         h = F.relu(self.conv2(h))
         h = F.relu(self.conv3(h))
         out = self.conv_out(h)
+
         return out
